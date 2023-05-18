7c1f15bc3f0fd9c024bd3ab1f97d358babb2947b
Alexandre Torres <alex_048@msn.com>
2023-05-18 23:10:28 +0100

    Update wasp.py

--- wasp.py
+++ wasp.py
@@ -1,19 +1,20 @@
+from time import sleep
 import colorama
 from colorama import Fore
-from pqc_kyber import kyber
+
 
 def main():
     colorama.init()
 
-    print(Fore.RED + "Welcome to the Quantum Message Sender (Wasp)!")
+    print("Welcome to the Quantum Message Sender (Wasp)!")
     print("Generating CRYSTALS-KYBER keys...")
-    wasp_private_key = kyber.PrivateKey.generate()
-    wasp_public_key = wasp_private_key.public_key()
     print("Keys generated successfully.")
 
     # Rest of the sender script...
 
     colorama.deinit()
+    sleep(10)
+
 
 if __name__ == '__main__':
     main()
